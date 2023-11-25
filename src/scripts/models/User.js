220036746bb14532d554639514b097867efe6722
butong <anggi21.af@gmail.com>
2023-11-26 04:08:47 +0700

    add new schema for user

--- src/scripts/models/User.js
+++ src/scripts/models/User.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ['employer', 'job_seeker'], required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String },
   fullName: { type: String },
-  phoneNumber: { type: String },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, validate: /^\d+$/ },
+  role: { type: String, enum: ['employer', 'job_seeker'], required: true },
+  password: { type: String, required: true },
+  address: { type: String, maxlength: 255 },
 });
 
 const User = mongoose.model('User', userSchema);
